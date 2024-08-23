d38f09beded48932a17dc4173a7ee75146c90fe7
kabaniery <mrscorpio93@gmail.com>
2024-08-23 21:15:17 +0300

    Установка БД

--- repricerDjango/settings.py
+++ repricerDjango/settings.py
@@ -36,6 +36,7 @@ SECRET_KEY = 'django-insecure-7%%u&y)7gx6_r9zf4a#7j7kx^(jbt#ml5!t1bw#gsly&6it+r)
 DEBUG = True
 
 ALLOWED_HOSTS = ["repriser-xreronxi.amvera.io", "*"]
+CSRF_TRUSTED_ORIGINS = ["repriser-xreronxi.amvera.io"]
 
 # Application definition
 
