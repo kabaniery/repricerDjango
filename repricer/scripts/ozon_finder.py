57125e47e81a0bef1d3c62c9dec2161e5c085c4d
kabaniery <mrscorpio93@gmail.com>
2024-08-24 03:41:19 +0300

    чиним

--- repricer/scripts/ozon_finder.py
+++ repricer/scripts/ozon_finder.py
@@ -19,12 +19,12 @@ import repricer.models
 
 def get_driver():
     chrome_options = Options()
-    #chrome_options.add_argument("--headless")
-    #chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
     #chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-
+    chrome_options.add_argument("--remote-debugging-port=9222")
     chrome_options.add_argument("--window-size=1920x1080")
     chrome_options.add_argument("--start-maximized")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
