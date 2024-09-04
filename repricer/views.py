c89fac526030b18480e32018a1f7fa139481240d
kabaniery <mrscorpio93@gmail.com>
2024-09-04 12:10:29 +0300

    Новая идея

--- repricer/views.py
+++ repricer/views.py
@@ -161,6 +161,7 @@ def example(request):
         temp.write(driver.title)
     elem = driver.find_element(By.TAG_NAME, "html").find_element(By.TAG_NAME, "body").find_element(By.TAG_NAME, "div").find_element(By.TAG_NAME, "div")
     elem = elem.find_element(By.TAG_NAME, "div").find_elements(By.TAG_NAME, "div")
+    elem[2].find_element(By.TAG_NAME, "button").click()
     print(elem[2].get_attribute("class"))
     driver.close()
     return HttpResponse(code)
