0c70ca26b04453802bf5d641ed29fa878ef14a71
kabaniery <mrscorpio93@gmail.com>
2024-09-20 14:25:58 +0300

    Selenium test

--- TEST.py
+++ TEST.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
+    options.add_argument("--remote-debugging-port=9222")
 
     service = Service("/usr/bin/google-chrome")
 
