0658ae33ef437d0721934df0738f24b0ea84d8b0
kabaniery <mrscorpio93@gmail.com>
2024-09-20 14:35:51 +0300

    Selenium test

--- TEST.py
+++ TEST.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 
 if __name__ == "__main__":
     options = Options()
-    options.add_argument("--headless")
+    #options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
