d89b8e2b66b31a022ad8b804df998e9b57542f80
kabaniery <mrscorpio93@gmail.com>
2024-10-29 13:57:13 +0300

    Фиксы

--- windowServer.py
+++ windowServer.py
@@ -5,13 +5,13 @@ from waitress import serve
 
 
 from manager_queue import get_queue, set_queue
-from repricerDjango import wsgi
 import multiprocessing
 
 if __name__ == "__main__":
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'repricerDjango.settings')
     django.setup()
     from ChromeController.ProcessManager import Manager
+    from repricerDjango import wsgi
     man = multiprocessing.Manager()
     set_queue(man.Queue())
     manager = Manager(10, get_queue())
