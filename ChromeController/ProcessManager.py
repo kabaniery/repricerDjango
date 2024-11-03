5a9f127c9b8e048e2fa30b888669603532b327e9
kabaniery <mrscorpio93@gmail.com>
2024-11-03 21:21:34 +0300

    Кажется, починка

--- ChromeController/ProcessManager.py
+++ ChromeController/ProcessManager.py
@@ -7,6 +7,7 @@ import time
 import redis
 from datetime import datetime, timezone
 
+from pyvirtualdisplay import Display
 from tortoise.transactions import in_transaction
 
 from ChromeController.Controller import SeleniumManager
@@ -24,6 +25,7 @@ async def save_client(client):
 class Manager(multiprocessing.Process):
     def __init__(self, count_process: int):
         super().__init__()
+        self.display = None
         self.putQueue = None
         self.count = count_process
         self.threads = list()
@@ -32,6 +34,7 @@ class Manager(multiprocessing.Process):
         self.manager = None
 
     def __del__(self):
+        self.display.stop()
         self.logger.warning("process stopped?")
 
     def run(self):
@@ -43,8 +46,8 @@ class Manager(multiprocessing.Process):
 
         self.logger.info(f"Process started with {self.count} threads")
         self.broker = redis.StrictRedis(host='localhost', port=6379, db=0)
-        # display = Display(visible=False, size=(1920, 1080))
-        # display.start()
+        self.display = Display(visible=False, size=(1920, 1080))
+        self.display.start()
         it = 0
         while True:
             time.sleep(3)
