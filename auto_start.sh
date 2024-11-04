d9572b8a9a768cebbfd09211904b7c352b0a6f8f
kabaniery <mrscorpio93@gmail.com>
2024-11-05 00:30:15 +0300

    Кажется, починка

--- auto_start.sh
+++ auto_start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 DOMAIN="api-seller.ozon.ru"
+OTHDOMAIN="ozon.ru"
+
 
 GATEWAY="10.0.0.1"
 INTERFACE="ens3"
@@ -18,6 +20,18 @@ for IP in $IP_ADDRESSES; do
   sudo ip route add "$IP" via "$GATEWAY" dev "$INTERFACE" onlink src "$SRC_IP"
 done
 
+# Получение IP-адресов для OTHDOMAIN
+IP_ADDRESSES_OTHDOMAIN=$(dig +short "$OTHDOMAIN")
+
+if [ -z "$IP_ADDRESSES_OTHDOMAIN" ]; then
+  echo "Не удалось получить IP-адреса для домена $OTHDOMAIN"
+  exit 1
+fi
+
+for IP in $IP_ADDRESSES_OTHDOMAIN; do
+  echo "Добавление маршрута для $IP (домен $OTHDOMAIN)"
+  sudo ip route add "$IP" via "$GATEWAY" dev "$INTERFACE" onlink src "$SRC_IP"
+done
 
 source venv/bin/activate
 python windowServer.py
